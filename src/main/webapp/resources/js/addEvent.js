0e87c6a287f0cf68c32f7a820f2326271636dcbe
kongeeee <rlwjd25@naver.com>
2020-07-23 17:00:46 +0900

    view page 작성

---
+++ src/main/webapp/resources/js/addEvent.js
@@ -0,0 +1,105 @@
+var eventModal = $('#eventModal');
+
+var modalTitle = $('.modal-title');
+var editAllDay = $('#allDay');
+var editTitle = $('#title');
+var editStart = $('#start');
+var editEnd = $('#end');
+var editType = $('#type');
+var editColor = $('#color');
+var editDesc = $('#description');
+
+var addBtnContainer = $('.modalBtnContainer-addEvent');
+var modifyBtnContainer = $('.modalBtnContainer-modifyEvent');
+
+
+/* ****************
+ *  새로운 일정 생성
+ * ************** */
+var newEvent = function (start, end, eventType) {
+
+    $("#contextMenu").hide(); //메뉴 숨김
+
+    modalTitle.html('새로운 일정');
+    editType.val(eventType).prop('selected', true);
+    editTitle.val('');
+    editStart.val(start);
+    editEnd.val(end);
+    editDesc.val('');
+    
+    addBtnContainer.show();
+    modifyBtnContainer.hide();
+    eventModal.modal('show');
+
+    /******** 임시 RAMDON ID - 실제 DB 연동시 삭제 **********/
+    var eventId = 1 + Math.floor(Math.random() * 1000);
+    /******** 임시 RAMDON ID - 실제 DB 연동시 삭제 **********/
+
+    //새로운 일정 저장버튼 클릭
+    $('#Eventwrite').unbind();
+    $('#Eventwrite').on('click', function () {
+
+        var eventData = {
+            _id: eventId,
+            title: editTitle.val(),
+            start: editStart.val(),
+            end: editEnd.val(),
+            description: editDesc.val(),
+            type: editType.val(),
+            backgroundColor: editColor.val(),
+            textColor: '#ffffff',
+            allDay: false
+        };
+
+        if (eventData.start > eventData.end) {
+            alert('끝나는 날짜가 앞설 수 없습니다.');
+            return false;
+        }
+
+        if (eventData.title === '') {
+            alert('일정명은 필수입니다.');
+            return false;
+        }
+
+        var realEndDay;
+
+        if (editAllDay.is(':checked')) {
+            eventData.start = moment(eventData.start).format('YYYY-MM-DD');
+            //render시 날짜표기수정
+            eventData.end = moment(eventData.end).add(1, 'days').format('YYYY-MM-DD');
+            //DB에 넣을때(선택)
+            realEndDay = moment(eventData.end).format('YYYY-MM-DD');
+
+            eventData.allDay = true;
+        }
+
+        $("#calendar").fullCalendar('renderEvent', eventData, true);
+        eventModal.find('input, textarea').val('');
+        editAllDay.prop('checked', false);
+        eventModal.modal('hide');
+
+        //새로운 일정 저장
+        $.ajax({
+            type: "post",
+            //url: "http://192.168.0.54:8080/write",
+            url : "dlwlrma.jsp",
+            data : jtoq(eventData),
+            success: function (response) {
+                //console.log(jtoq(eventData));
+                //DB연동시 중복이벤트 방지를 위한
+                //$('#calendar').fullCalendar('removeEvents');
+                //$('#calendar').fullCalendar('refetchEvents');
+            }
+        });
+    });
+};
+
+//제이슨을 쿼리스트링으로
+jtoq = function(obj){
+    var str = [];
+    for(var p in obj)
+        if(obj.hasOwnProperty(p)){
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+        return str.join("&");
+};
